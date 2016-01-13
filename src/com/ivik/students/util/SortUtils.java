75aa887ceceef4516433c232aeb4fe698dfa732b
Danman85 <d.versantvoort@hotmail.com>
2016-01-13 15:50:27 +0100

    update 1

--- src/com/ivik/students/util/SortUtils.java
+++ src/com/ivik/students/util/SortUtils.java
@@ -4,8 +4,6 @@ import com.ivik.students.model.Student;
 
 public class SortUtils{
 
-    static boolean sorted = false;
-
     public static void sortStudents(Student[] students){
 
         Student[] temp;
@@ -13,9 +11,11 @@ public class SortUtils{
         Student alleen = new Student("placeholder", 23.4);
         temp = new Student[]{alleen};
 
-        while (sorted = false) {
+        boolean sorted = false;
+
+        while (!sorted) {
             sorted = true;
-            for (int n = 0; n < students.length; n++) {
+            for (int n = 0; n < students.length-1; n++) {
                 if (students[n].getScore() < students[n + 1].getScore()) {
                     temp[0] = students[n+1];
                     students[n+1] = students[n];
--- src/opdrachten/Opdracht2.java
+++ src/opdrachten/Opdracht2.java
@@ -8,14 +8,17 @@ public class Opdracht2 {
 
         int[] array1 = {5, 20, 40, 15, 1, 30, 100, 6, 300, 23, 48};
 
-        int[] array2 = Arrays.copyOfRange(array1, 3, 8);
-            for (int m : array2) {
-                System.out.print(m + " ");
-            }
-
-        Arrays.sort(array1);
-        for (int n : array1) {
+        int[] array2 = Arrays.copyOfRange(array1, 0, 10);
+        Arrays.sort(array2);
+        for (int n : array2) {
             System.out.print(n + " ");
+        }
+
+        System.out.printf("%n");
+
+        int[] array3 = Arrays.copyOfRange(array1, 3, 9);
+        for (int m : array3) {
+            System.out.print(m + " ");
         }
     }
 }
